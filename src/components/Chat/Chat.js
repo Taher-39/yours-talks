ef9ef1f7cb51c3ab25beae964d1bdd438b537a94
Abu <abut70243@gmail.com>
2022-01-26 21:02:38 +0600

    after design part

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,80 @@
 import React from "react";
-import './Chat.css'
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
 const Chat = () => {
-  return <div className="chat">
-    Chat area
-  </div>; 
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHhHmXIoi6iDDBTm35yGRc9oNLrlBDj36KUQ&usqp=CAU" />
+
+        <div className="chat__headerInfo">
+          <h4>Room Name</h4>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Taher</span>
+          How are you.
+          <span className="chat__timestamp">{new Date().toDateString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__receiverName">Forhad</span>
+          Alhamdulillah, Fine. You?
+          <span className="chat__timestamp">{new Date().toDateString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Taher</span>
+          Alhamdulillah. Is going school today?
+          <span className="chat__timestamp">{new Date().toDateString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__receiverName">Forhad</span>
+          No, School is off today.
+          <span className="chat__timestamp">{new Date().toDateString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+
+        <form>
+          <input
+            type="text"
+            name="massage"
+            id="massage"
+            placeholder="Type a massage"
+          />
+          <button type="submit">Send</button>
+        </form>
+        
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -3,11 +3,22 @@ import { Avatar } from "@material-ui/core";
 import './sidebarChat.css'
 
 const SidebarChat = () => {
+  const avatarImgs = [
+    {
+      imgLink: "https://pbs.twimg.com/media/DXqHKZ0WsAACzLT.jpg"
+    },
+    {
+      imgLink: "https://preview.redd.it/v00q2i2r2wu21.jpg?width=640&crop=smart&auto=webp&s=3a47788729c08f2b152e4792b2f27489809d9a67"
+    },
+    {
+      imgLink: "https://preview.redd.it/v00q2i2r2wu21.jpg?width=640&crop=smart&auto=webp&s=3a47788729c08f2b152e4792b2f27489809d9a67"
+    },
+  ]
   return (
     <div className="sidebarChat">
-      <Avatar src="https://avatars.githubusercontent.com/u/53148971?s=96&v=4" />
+      <Avatar src="https://pbs.twimg.com/media/DXqHKZ0WsAACzLT.jpg" />
       <div className="sidebarChat-info">
-        <h4>Room Name</h4>
+        <h2>Room Name</h2>
         <p>This room massage is important</p>
       </div>
     </div>
